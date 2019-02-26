ecbc5b00a883b16aa0ae264ced16c7c54d150dda
jonatanhost <jonatan.h96@gmail.com>
2019-02-26 21:10:48 +0100

    Added user selection

--- 1_PreUpload.ps1
+++ 1_PreUpload.ps1
@@ -5,7 +5,19 @@ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ht
 
 Import-PSSession $Session -DisableNameChecking
 
-#Ask user for input about language choise
+#Prompt user for input about scope on who to run script on
+$ScopeInput = Read-Host "Change language on All(A) users or specific(S) users?"
+
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'Textfile (*.txt)|*.txt'
+}
+$null = $FileBrowser.ShowDialog()
+
+$Users = Get-Content $FileBrowser
+
+#Prompt user for input about language choise
 $LanguageInput = Read-Host "English(en) or Swedish(se) as regional setting for all users mailboxes?"
 
 switch ($LanguageInput)
@@ -16,4 +28,14 @@ switch ($LanguageInput)
     swedish {$LanguageCode = 1053}
 }
 
-Get-Mailbox -Filter {Name -notlike '*discover*'} | Set-MailboxRegionalConfiguration -Language $LanguageCode -TimeZone "W. Europe Standard Time" -LocalizeDefaultFolderName
+if($ScopeInput -eq "A")
+{
+    Get-Mailbox -Filter {Name -notlike '*discover*'} | Set-MailboxRegionalConfiguration -Language $LanguageCode -TimeZone "W. Europe Standard Time" -LocalizeDefaultFolderName
+}elseif ($ScopeInput -eq "S") {
+    foreach($User in $Users){
+        Get-Mailbox -Filter {Name -like '*$User*'} | Set-MailboxRegionalConfiguration -Language $LanguageCode -TimeZone "W. Europe Standard Time" -LocalizeDefaultFolderName
+    }
+    
+}
+
+Write-Host "All done!"
